e45aa8af73b114b330a232282d4866a2a6399b8d
poignant-minifier-0983 <davidfano@gmail.com>
2019-05-16 16:44:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,11 @@
 def line(arr)
 	new_list = ["The line is currently:"]
 	if arr.length == 0
-		"The line is currently empty."
+		puts "The line is currently empty."
 	else
 		arr.each_with_index.map do |name, index|
 			new_list << " #{index+1}. #{name.capitalize}"
 		end
 		new_list.join
 	end
-
 end
